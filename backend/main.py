8bdba08e09a979a682ecb37f250149930a170849
Anas-Progk <anasanasmohd431@gmail.com>
2026-07-29 21:07:50 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -40,6 +40,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
+        "https://edu-lens-62uvjsvup-anas-progk1.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
